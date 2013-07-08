115f9e2da3f957e36c01d8af6aa912584d48c682
PEDRA <romain.pedra@gmail.com>
2013-07-08 13:49:12 +0200

    HTMLScores

--- Assets/www/listerScoresHTML.php
+++ Assets/www/listerScoresHTML.php
@@ -3,14 +3,16 @@ try
 {
 	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
     $bdd = new PDO('mysql:host=mysql51-45.perso;dbname=romainpeu1991', 'romainpeu1991', 'i667Uy36', $pdo_options);
+	$tableau = "<table>";
 	$req1 = $bdd->query('SELECT * FROM ExamUnity ORDER BY score DESC LIMIT 0,10');
-	$tableau = "";
 	while ($donnees = $req1->fetch()) {
-
-		$tableau .= "$donnees[pseudo];";
-		$tableau .= "$donnees[score];";
+		$tableau .= "<tr>";
+		$tableau .= "<td>$donnees[pseudo]</td>";
+		$tableau .= "<td>$donnees[score]</td>";
+		$tableau .= "</tr>";
 	
 	}
+	$tableau .= "</table>";
 	echo $tableau;
 
 
