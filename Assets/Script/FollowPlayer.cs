74d06d89c6b6749c0c1717dd168c0cff20d14955
mbonnet <maxime.bonnet.isart@gmail.com>
2013-07-09 12:02:56 +0200

    cam

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position=new Vector3(player.position.x, transform.position.y, transform.position.z);
+	}
+}
