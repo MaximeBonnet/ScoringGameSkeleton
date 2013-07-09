dc3fe2b0c7d0546da20c6fa2495fb172a12a4cb4
PEDRA <romain.pedra@gmail.com>
2013-07-09 10:11:56 +0200

    Last Build

--- Assets/Script/GUI/GuiDisplayScores.cs
+++ Assets/Script/GUI/GuiDisplayScores.cs
@@ -25,15 +25,16 @@ public class GuiDisplayScores : MonoBehaviour {
         if (scores != null)
         {
             //Debug.Log(scores.Count);
-            foreach (var pair in scores)
+            /*-foreach (var pair in scores)
             {
                 //Debug.Log(pair.Key+" "+pair.Value);
                 GUI.Label(new Rect(2*Screen.width/10, i * 25 + 50, Screen.width, 100), pair.Key + " " + pair.Value);
                 i++;
-            }
+            }*/
+            ScoreTable(scores);
         }
 
-        if (GUI.Button(new Rect(2 * Screen.width / 10f, 8 * Screen.height / 10f, Screen.width / 10f, Screen.height / 10f),"Reload Scores"))
+        if (GUI.Button(new Rect(Screen.width / 15f, 5 * Screen.height / 10f, Screen.width / 10f, Screen.height / 10f),"Reload Scores"))
         {
             scoreLoaded = false;
             scores = null;
@@ -42,4 +43,16 @@ public class GuiDisplayScores : MonoBehaviour {
             
         }
 	}
-}
+
+    void ScoreTable(Dictionary<string,int> scores) {
+        float win=Screen.width*0.6f;
+        float w1=win*0.35f; 
+        float w2=win*0.15f;
+        foreach (var pair in scores) {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(pair.Key, GUILayout.Width(w1));
+            GUILayout.Label(pair.Value.ToString(), GUILayout.Width(w2));
+            GUILayout.EndHorizontal();                  
+        }
+    }
+} 
