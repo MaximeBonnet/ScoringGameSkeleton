0ee3bb0f8c6edf511edfb1e80629bce0c1e02b54
mbonnet <maxime.bonnet.isart@gmail.com>
2013-07-09 12:32:56 +0200

    sfdg

--- Assets/Script/GUI/GuiName.cs
+++ Assets/Script/GUI/GuiName.cs
@@ -15,6 +15,13 @@ public class GuiName : MonoBehaviour {
 		}
 	}
 	
+	void Update(){
+		if(Input.GetButtonDown("Jump")&&name!=""){
+			PlayerPrefs.SetString("name", name);
+			Application.LoadLevel("Game");
+		}
+	}
+	
 	void OnGUI () {
 		name=GUI.TextField(new Rect(Screen.width/2-250,Screen.height-25, 250, 25), name);
 		if(GUI.Button(new Rect(Screen.width/2,Screen.height-25, 50, 25), "ok")&&name!=""){
